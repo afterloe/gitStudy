06d97e8b2f56b196a4de843fb898f011770c0354
afterloe <lm6289511@gmail.com>
2018-05-17 14:19:13 +0800

    feat: 项目初始化

---
+++ src/main/java/cn/cityworks/traffic/TrafficApplication.java
@@ -0,0 +1,12 @@
+package cn.cityworks.traffic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrafficApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrafficApplication.class, args);
+	}
+}
